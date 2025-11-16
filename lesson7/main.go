a1e80753d1f30cb26c1a47e45ed674b6268c78dd
ESKL <Elizaveta.sokolova.01@mail.ru>
2025-11-16 11:15:48 +0300

    Func declaration

--- lesson7/main.go
+++ lesson7/main.go
@@ -2,20 +2,38 @@ package main
 
 import "fmt"
 
-func Cube(x int) (res int) {
-	res = x*x*x
-	return res
+var funcVar func(int) int
+
+func Sq(x int) int{
+	return x*x
 }
 
 func main() {
-	res := Sum(1, 2, 3, 4)
-	fmt.Println(res)
+	funcVar = Sq
 }
 
+///////////lessonPractics://////////////
+
+func input2ints (x int, y int){
+	fmt.Printf("Два числа: %d и %d", x, y )
+}
+
+func returns2ints()(x int, y int){
+	return 7, 10;
+}
+
+func theirdFunc(){
+	input2ints(returns2ints())
+}
 
 func Sum(x ...int) (res int){
 	for _, v := range x{
 		res = v
 	} 
 	return 
+}
+
+func Cube(x int) (res int) {
+	res = x*x*x
+	return res
 }
