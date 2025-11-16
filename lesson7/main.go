75df9985fd0036bef3268d0412a6406886531813
ESKL <Elizaveta.sokolova.01@mail.ru>
2025-11-16 10:47:58 +0300

    func(x)

---
+++ lesson7/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func Cube(x int) (res int) {
+	res = x*x*x
+	return res
+}
+
+func main() {
+	res := Sum(1, 2, 3, 4)
+	fmt.Println(res)
+}
+
+
+func Sum(x ...int) int{
+	for 
+	return 
+}
